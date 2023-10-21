3e27471c88b2e9bbccfb31f8ad151ac20e930170
Anna Roses <anna.roses.marti@gmail.com>
2023-10-21 19:58:37 +0200

    Refactor API fetch, make a getPokemonApi gunction

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,11 @@
-interface Pokemons {
-  id: number;
+export interface PokemonsList {
+  count: number;
+  next: string;
+  previous: string;
+  results: PokemonName[];
+}
+
+export interface PokemonName {
   name: string;
   url: string;
 }
-
-export default Pokemons;
