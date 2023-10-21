10ff2e84cec5e53299c734009d3b07d62aea0b40
Anna Roses <anna.roses.marti@gmail.com>
2023-10-21 20:07:36 +0200

    Add test for getPokeApi

---
+++ src/data/getPokemonApi/getPokemonApi.test.ts
@@ -0,0 +1,19 @@
+import { getPokemonApi } from "./getPokemonApi.js";
+import mockFetch from "../mockFetch.js";
+import mockData from "../mockData.json";
+
+describe("Given a fetch() method", () => {
+  describe("When it receives an 'https://pokeapi.co/api/v2/pokemon?limit=10&offset='", () => {
+    test("Then it should return 1 pokemon name", async () => {
+      const urlApi = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=";
+      const pokemonOffset = 20;
+      const testName = "bulbasaur";
+      window.fetch = mockFetch(mockData);
+
+      const pokemonInfo = await getPokemonApi(urlApi, pokemonOffset);
+      expect(pokemonInfo.results[0].name).toBe(testName);
+
+      console.log(pokemonInfo);
+    });
+  });
+});
