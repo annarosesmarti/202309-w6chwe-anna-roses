d6970e393d7bcb9135dcbb3587484ee0fa268724
Anna Roses <anna.roses.marti@gmail.com>
2023-10-22 21:12:01 +0200

    Add getPokemonById function

--- src/data/getPokemonApi/getPokemonApi.test.ts
+++ src/data/getPokemonApi/getPokemonApi.test.ts
@@ -3,14 +3,13 @@ import mockFetch from "../mockFetch.js";
 import mockData from "../mockData.json";
 
 describe("Given a getPokemonApi() function", () => {
-  describe("When it receives an 'https://pokeapi.co/api/v2/pokemon?limit=10&offset='", () => {
+  describe("When it receives an 'https://pokeapi.co/api/v2/pokemon/'", () => {
     test("Then it should return 1 pokemon name", async () => {
-      const urlApi = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=";
-      const pokemonOffset = 20;
+      const urlApi = "https://pokeapi.co/api/v2/pokemon/";
       const testName = "bulbasaur";
       window.fetch = mockFetch(mockData);
 
-      const pokemonInfo = await getPokemonApi(urlApi, pokemonOffset);
+      const pokemonInfo = await getPokemonApi(urlApi);
       expect(pokemonInfo.results[0].name).toBe(testName);
     });
   });
